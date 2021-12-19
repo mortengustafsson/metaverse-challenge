32e4c0f6793986d18a220fb0333fde3d5ead8ed6
Morten Gustafsson <morten@mortengustafsson.dk>
2021-12-19 01:21:17 +0100

    Fix word break

--- src/components/Message.js
+++ src/components/Message.js
@@ -10,7 +10,7 @@ const Message = ({ message }) => {
     return (
         <motion.div
             className={`${isUserMessage ? 'origin-right' : 'origin-left'} my-0`}
-            whileHover={{ scale: 1.2 }}
+            whileHover={{ scale: 1.15 }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ type: "spring", duration: 0.5, stiffness: 300 }}
@@ -22,7 +22,7 @@ const Message = ({ message }) => {
                 <div className="flex flex-col max-w-[70%] w-auto">
                     <p className={`pb-2 text-sm ${isUserMessage ? 'text-right' : 'text-left'}`}> {message.get('username')}
                     </p>
-                    <div className={`flex space-x-4 p-3 rounded-lg break-all ${isUserMessage
+                    <div className={`flex space-x-4 p-3 rounded-lg breakWord ${isUserMessage
                         ? "rounded-tr-none bg-pink-500"
                         : "rounded-tl-none bg-blue-500"}`
                     }>
