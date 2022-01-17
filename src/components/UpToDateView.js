7b0280d94fc2f9f0f3f9d63e8d8a4d18b3705bcd
Morten Gustafsson <morten@mortengustafsson.dk>
2022-01-17 14:13:49 +0100

    Add UpToDateView

---
+++ src/components/UpToDateView.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UpToDateView = () => {
+    return (<div className="m-10 pt-8 text-lg text-center"><span className=" font-bold text-xl">Congratulation</span> - you&lsquo;re up to date.</div>)
+}
+
+export default UpToDateView;
