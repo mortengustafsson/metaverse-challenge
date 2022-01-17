0550e7835f56e3b5677f2d2ff98a2f5b64d0c24c
Morten Gustafsson <morten@mortengustafsson.dk>
2022-01-17 14:13:07 +0100

    Remove file

--- src/components/UptoDateView.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UpToDateView = () => {
-    return (<div className="m-10 pt-8 text-lg text-center"><span className=" font-bold text-xl">Congratulation</span> - you&lsquo;re up to date.</div>)
-}
-
-export default UpToDateView;
